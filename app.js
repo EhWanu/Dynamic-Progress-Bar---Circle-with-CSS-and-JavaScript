2f685ef98e913b5b3eb5f74576d1dde171e81db8
EhWanu <cameronjshadgett@gmail.com>
2021-10-09 21:06:53 -0400

    clearInterval

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const progress = document.querySelector('.progress')
 const loading = document.querySelector('.loading')
 
 let i = 0
-const fakeUploadPercentage = [0, 10, 25, 50, 75, 100,]
+const fakeUploadPercentage = [ 10, 25, 50, 75, 100,]
 
 const interval = setInterval(() => {
     progress.style.width = fakeUploadPercentage[i] + '%'
     loading.innerHTML = fakeUploadPercentage[i] + '%'
     i++
+    if(i== fakeUploadPercentage.length){
+        clearInterval(interval)
+        loading.innerHTML = 'Complete!'
+    }
 }, 1000)
