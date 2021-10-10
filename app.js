1bdb4c3132b07702c85f28e45facf629f8be634f
EhWanu <cameronjshadgett@gmail.com>
2021-10-10 17:06:41 -0400

    svg red circle and commented out code

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const progress = document.querySelector('.progress')
-const loading = document.querySelector('.loading')
+// const progress = document.querySelector('.progress')
+// const loading = document.querySelector('.loading')
 
-let i = 0
-const fakeUploadPercentage = [ 10, 25, 50, 75, 100,]
+// let i = 0
+// const fakeUploadPercentage = [ 10, 25, 50, 75, 100,]
 
-const interval = setInterval(() => {
-    progress.style.width = fakeUploadPercentage[i] + '%'
-    loading.innerHTML = fakeUploadPercentage[i] + '%'
-    i++
-    if(i== fakeUploadPercentage.length){
-        clearInterval(interval)
-        loading.innerHTML = 'Complete!'
-    }
-}, 1000)
+// const interval = setInterval(() => {
+//     progress.style.width = fakeUploadPercentage[i] + '%'
+//     loading.innerHTML = fakeUploadPercentage[i] + '%'
+//     i++
+//     if(i== fakeUploadPercentage.length){
+//         clearInterval(interval)
+//         loading.innerHTML = 'Complete!'
+//     }
+// }, 1000)
