360f75fdfe3d3b3bba908217d57aa5b4dbff2592
EhWanu <cameronjshadgett@gmail.com>
2021-10-09 21:03:01 -0400

    setInterval loading animation

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const progress = document.querySelector('.progress')
+const loading = document.querySelector('.loading')
+
+let i = 0
+const fakeUploadPercentage = [0, 10, 25, 50, 75, 100,]
+
 const interval = setInterval(() => {
-    console.log('hello')
-}, 1000);
+    progress.style.width = `${fakeUploadPercentage[i]}%`
+    
+    i++
+}, 1000)
